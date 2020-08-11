daf3caa7ef80fc8c877a10779d45b03d3dc26e20
mihaichiciu <mihai99918@gmail.com>
2020-08-11 15:21:08 +0300

    finished auth flow

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport'); // ce este in services/passport.js este executat, nu ma intereseaza sa preiau ceva de acolo
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 de zile
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+const authRoutes = require('./routes/auth');
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send({ good: 'morning' });
-});
+app.use('/', authRoutes);
 
 app.listen(PORT, () => {
   console.log('Server has started on port:', PORT);
---
+++ services/passport.js
@@ -0,0 +1,44 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const foundUser = await User.findById(id);
+    done(null, foundUser);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const user = await User.findOne({ googleId: profile.id });
+        if (user) {
+          done(null, user);
+        } else {
+          const newUser = await User.create({ googleId: profile.id });
+          done(null, newUser);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  )
+);
+
+module.exports = passport;
